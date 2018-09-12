fc0e6dda5d4b8fbc0d2f17e6408ed105ba6f8741
Thomas Sarlin <thomas.sarlin@gmail.com>
2018-09-12 17:43:32 +0200

    stuff

--- Makefile
+++ Makefile
@@ -3,10 +3,25 @@ CC = gcc
 CFLAGS = -std=gnu11 -Wall -Wmissing-declarations -Wreturn-type -Wparentheses -Wunused -Wold-style-definition -Wundef -Wshadow -Wstrict-prototypes -Wswitch-default -Wunreachable-code
 
 SOURCES = mpasswdsort.c userInfo.c linkedList.c
+
 TARGET = mpasswdsort
 
 all:
-		$(CC) $(CFLAGS) $(SOURCES) -o $(TARGET)
+		$(CC) -Wall -g $(SOURCES) -o $(TARGET)
+
+main:mpasswdsort.o userInfo.o linkedList.o
+		$(CC) -Wall -g mpasswdsort.o userInfo.o linkedList.o -o $(TARGET)
+
+mpasswdsort.o:mpasswdsort.c mpasswdsort.h
+	$(CC) $(CFLAGS) -c mpasswdsort.c
+
+userInfo.o: userInfo.c userInfo.h
+	$(CC) $(CFLAGS) -c userInfo.c
+
+linkedList.o:linkedList.c linkedList.h
+	$(CC) $(CFLAGS) -c linkedList.c 
+
+
 all33:
 	gcc mpasswdsort.c userInfo.c linkedList.c  -Wall -g -o mpasswdsort
 
--- mpasswdsort.c
+++ mpasswdsort.c
@@ -5,6 +5,7 @@
 #include "userInfo.h"
 #include <errno.h>
 #include <string.h>
+
 void printUsrList(list*);
 void printErrorList(list*);
 void checkFile(char* file);
@@ -15,6 +16,7 @@ bool hasEmptyFields(char* string);
 char* getEmptyFieldType(char* string);
 unsigned int getUID(char* string);
 unsigned int getGID(char* string);
+
 int main(int argc, char *argv[]){
 		
 	printf("Initiating list");
@@ -34,70 +36,49 @@ int main(int argc, char *argv[]){
 	
 	return 0;
 };
+
 void checkFile(char* file){
 	FILE* fp;
 	fp=fopen(file,"r");
 	char* string;
-	unsigned int gid,uid;
-	char uname[32],pass[100],geo[300],dir[300],shell[300];
 	size_t len = 0;
 	ssize_t read;
 	int lineCount=1;
-	//unsigned int uid;
-	//char* uname;
 	while((read= getline(&string,&len,fp))!=-1){
 		char* temp=malloc(strlen(string)+1);
 		strcpy(temp,string);
 
-		sscanf(temp,"%[^:]:%[^:]:%u:%u:%[^:]:%[^:]:%[^:]:",uname,pass,&uid,&gid,geo,dir,shell);
-		printf("%s %s %d %d %s %s %s\n",uname,pass,uid,gid,geo,dir,shell);
 		if(strlen(temp)==1)
-			printf("Line %d: Encountered a <BLANKLINE>\n",lineCount);
+			printf("Line %d: Encountered a <BLANKLINE>\n"
+					,lineCount);
 		else if(!validFormat(string)){
-			printf("Line %d: Invalid format: %s\n",lineCount,string);
-		}else if(!validUID(string)){
-			printf("Line %d: UID has to be a number. Got: \"%d\"",lineCount,getUID(string));
-		}else if(!validGID){
-			printf("Line %d: GID has to be a positive number. Got \"%d\""
-					,lineCount,getGID(string));
-		}else if(!hasEmptyFields){
-			printf("Line %d: The %s field cannot be empty",lineCount,getEmptyFieldType(string));
+			printf("Line %d: Invalid format: %s\n"
+					,lineCount,string);
 		}
 		lineCount++;
+		free(temp);
 	}
-	/*
-	fseek(fp,0,SEEK_END);
-	long fsize = ftell(fp);
-	char* string = malloc(fsize+1);
-	printf("%s\n",argv[1]);
-	fseek(fp,0,SEEK_SET);
-	fread(string,fsize,1,fp);
-	*/
 	fclose(fp);
 
 }
 
 bool validFormat(char* string){
 	int count = 0;
-	char* token= strtok(string,":");
-	while(token!=NULL){
+	while(strsep(&string,":")!=NULL)
 		count++;
-		token=strtok(NULL,":");	
-	}
-	
 	return count==7;
 };
 bool validUID(char* string){
-	return string!="";
+	return true;
 };
 bool validGID(char* string){
-	return string!="";
+	return true;
 };
 unsigned int getGID(char* string){
 	return 0;
 };
 bool hasEmptyFields(char* string){
-	return string!="";
+	return true;
 };
 char* getEmptyFieldType(char* string){
 	return string;
@@ -111,7 +92,7 @@ void printUsrList(list* l){
 	while(list_hasNext(n)){
 		n=list_next(n);
 		uinfo=list_inspect(n);
-		printf("%d:%s\n"
+		printf("%u:%s\n"
 			,userInfo_getUid(uinfo)
 			,userInfo_getUname(uinfo));
 	}	
